5fd3e8e0f22cb4ba4db716001e5c1f612658ef16
Davron Gaziev <davron1104@yandex.com>
2023-06-05 23:58:41 +0500

    Second

--- script.js
+++ script.js
@@ -1,37 +1,80 @@
 'use strict';
 
-class Rectangle{
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
-    }
+// class Rectangle{
+//     constructor(height, width) {
+//         this.height = height;
+//         this.width = width;
+//     }
 
-    calcArea() {
-        return this.height * this.width;
-    }
-}
+//     calcArea() {
+//         return this.height * this.width;
+//     }
+// }
 
 
-class ColoredRectangleWithtext extends Rectangle {
-    constructor(height, width, text, bgColor) {
-        super(height, width);
-        this.text = text;
-        this.bgColor = bgColor;
+// class ColoredRectangleWithtext extends Rectangle {
+//     constructor(height, width, text, bgColor) {
+//         super(height, width);
+//         this.text = text;
+//         this.bgColor = bgColor;
 
-    }
+//     }
 
-    showMyProps() {
-        console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
-    }
-}
+//     showMyProps() {
+//         console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+//     }
+// }
 
-const div = new ColoredRectangleWithtext(25, 10, 'Hello World', 'red');
+// const div = new ColoredRectangleWithtext(25, 10, 'Hello World', 'red');
 
-div.showMyProps();
-console.log(div.calcArea());
+// div.showMyProps();
+// console.log(div.calcArea());
 
 // const square = new Rectangle(10, 10);
 // const long = new Rectangle(20, 100);
 
 // console.log(square.calcArea());
 // console.log(long.calcArea());
+
+
+
+class Rectangle {
+    constructor (height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea () {
+        return this.height * this.width;
+    }
+}
+
+// const square = new Rectangle (10, 10);
+// const anotherSquare = new Rectangle (30, 20);
+
+// console.log(square.calcArea());
+// console.log(anotherSquare.calcArea());
+
+class ColoredRectangleWithtext extends Rectangle {
+    constructor (height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`Текст: ${this.text}, цвет: ${this.bgColor} `);
+    }
+}
+
+const div = new ColoredRectangleWithtext(25, 10, 'Hello world', 'red');
+div.showMyProps();
+console.log(div.calcArea());
+
+
+
+
+
+
+
+
